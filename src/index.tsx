62a41cfc1a0eee3ef559b0f6b0d81ae93abe2a48
Jaiden Mispy <jaiden@mispy.me>
2018-07-02 04:17:53 +0800

    Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,10 @@ import './index.scss'
 
 const MAX_POKEMON = 493
 
-class Pokemon extends React.Component<{ number: number, out?: boolean }> {
+class Pokemon extends React.Component<{ number: number, out?: boolean, hidden?: boolean }> {
     render() {
-        const {number, out} = this.props
-        return <div style={`--frame1-url: url("../overworld/right/${number}.png"); --frame2-url: url("../overworld/right/frame2/${number}.png");` as any} className={`pokemon ${out ? "walkout" : "walkin"}`}/>
+        const {number, out, hidden} = this.props
+        return <div style={`--frame1-url: url("../overworld/right/${number}.png"); --frame2-url: url("../overworld/right/frame2/${number}.png");` as any} className={`pokemon ${out ? "walkout" : "walkin"}${hidden ? " hidden" : ""}`}/>
     }
 }
 
@@ -24,7 +24,8 @@ class Pokemon extends React.Component<{ number: number, out?: boolean }> {
 class Main extends React.Component {
     @observable prevPoke?: number
     @observable question: number = 1
-    @observable poke: number = 135//Math.floor(Math.random()*pokenames.en.length)
+    @observable poke: number = 98//Math.floor(Math.random()*pokenames.en.length)
+    @observable nextPoke: number = _.random(1, MAX_POKEMON)
     @observable hintCount: number = 0
     @observable input: string = ""
 
@@ -38,9 +39,10 @@ class Main extends React.Component {
 
     @action.bound onKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.key === "Enter") {
-            if (this.input === this.trueAnswer) {
+            if (this.input.toLowerCase() === this.trueAnswer.toLowerCase()) {
                 this.prevPoke = this.poke
-                this.poke = _.random(1, MAX_POKEMON)
+                this.poke = this.nextPoke
+                this.nextPoke = _.random(1, MAX_POKEMON)
                 this.question += 1
                 this.hintCount = 0
                 this.input = ""
@@ -63,18 +65,18 @@ class Main extends React.Component {
     @observable animElapsed: number = 0
 
     render() {
-        const {poke, prevPoke, japanese, input} = this
+        const {poke, prevPoke, nextPoke, japanese, input} = this
 
         return <div className="container text-center">
             <div className="runway">
                 <Pokemon number={poke} key={this.question}/>
                 {prevPoke && <Pokemon number={prevPoke} out={true} key={`prev-${this.question}`}/>}
+                {nextPoke && <Pokemon number={nextPoke} key={`next-${this.question}`} hidden={true}/>}
             </div>
             <div>#{poke} {japanese}</div>
             <input type="text" className={`form-control`} placeholder="romaji..." onKeyPress={this.onKeyPress} value={input} onInput={this.onInput}/>
-            <br/>
             <button className="btn btn-light text-secondary" onClick={this.onHint}>hint</button>
-            <div style={{height: "100px", marginTop: "20px"}}>
+            <div style={{height: "50px", marginTop: "10px"}}>
                 {_.range(0, Math.min(this.hintCount, this.japanese.length)).map(i => 
                     <p>{/*this.japanese[i] + wanakana.toRomaji(this.japanese[i]) + " " + */this.japanese.slice(0, i+1) + " " + wanakana.toRomaji(this.japanese.slice(0, i+1))}</p>
                 )}
