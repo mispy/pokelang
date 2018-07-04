579a08b39556dd2f2d523b1368a147cae6539a44
Jaiden Mispy <jaiden@mispy.me>
2018-07-04 22:49:59 +0800

    Fix up multiple choice bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {Helmet, HelmetData} from 'react-helmet'
-import Homepage from './Homepage'
 import {observable, action, computed} from 'mobx'
 import {observer} from 'mobx-react'
 import * as _ from 'lodash'
@@ -28,10 +26,24 @@ class Pokemon extends React.Component<{ number: number, out?: boolean, hidden?: 
 function splitKana(jp: string): string[] {
     const kana = []
 
-    const modifiers = ["ー", "ッ", "ャ", "ョ", "ュ", "ェ"]
+    const beforeModifiers = ["ッ"]
+    const afterModifiers = ["ー",  "ャ", "ョ", "ュ", "ェ"]
     
     for (let i = 0; i < jp.length; i++) {
-        kana.push(jp.slice(i, i+1))
+        let j = i
+        while (beforeModifiers.indexOf(jp[j]) !== -1) {
+            j += 1
+        }
+        while (afterModifiers.indexOf(jp[j+1]) !== -1) {
+            j += 1
+        }
+
+        if (j > i) {
+            kana.push(jp.slice(i, j+1))
+            i = j
+        } else {
+            kana.push(jp.slice(i, i+1))
+        }
     }
 
     return kana
@@ -41,7 +53,7 @@ function splitKana(jp: string): string[] {
 class Main extends React.Component {
     @observable prevPoke?: number
     @observable question: number = 1
-    @observable poke: number = 135//Math.floor(Math.random()*pokenames.en.length)
+    @observable poke: number = 113//Math.floor(Math.random()*pokenames.en.length)
     @observable nextPoke: number = _.random(1, MAX_POKEMON)
     @observable hintCount: number = 0
     @observable kanaIndex: number = 0
@@ -58,25 +70,20 @@ class Main extends React.Component {
         return this.kana[this.kanaIndex]
     }
 
-    @computed get allKana(): string[] {
-        return _.uniq(splitKana(pokenames.ja.join()))
-    }
-
-    @computed get options(): string[] {
-        return _.shuffle(_.sampleSize(this.allKana, 3).map(kana => wanakana.toRomaji(kana)).concat([this.correctOption]))
+    @computed get optionSet(): string[] {
+        return _.uniq(splitKana(pokenames.ja.join())).map(kana => wanakana.toRomaji(kana)).filter(r => r.length && r.match(/^\w+$/))
     }
 
     @computed get correctOption(): string {
         return wanakana.toRomaji(this.currentKana)
     }
 
+    @computed get options(): string[] {
+        return _.shuffle(_.sampleSize(this.optionSet, 3).concat([this.correctOption]))
+    }
+
     @action.bound chooseOption(option: string) {
         if (option === this.correctOption) {
-            this.kanaIndex += 1
-        }
-
-        // Skip things that aren't kana
-        while (wanakana.toRomaji(this.kana) === this.kana) {
             this.kanaIndex += 1
         }
 
@@ -96,13 +103,12 @@ class Main extends React.Component {
 
     render() {
         const {poke, prevPoke, nextPoke, japanese, kana, options, currentKana, kanaIndex} = this
-
-        for (const name of pokenames.ja) {
+        /*for (const name of pokenames.ja) {
             const trueRomaji = wanakana.toRomaji(name)
             const splitRomaji = splitKana(name).map(kana => wanakana.toRomaji(kana)).join("")
             if (trueRomaji !== splitRomaji)
                 console.log(name, trueRomaji, splitRomaji)
-        }
+        }*/
 
         return <div className="container text-center">
             <div className="runway">
@@ -112,9 +118,9 @@ class Main extends React.Component {
             </div>
             <div>{currentKana}</div>
             {options.map(option => 
-                <button className="btn btn-light text-secondary" onClick={e => this.chooseOption(option)}>{option}</button>
+                <button className="btn btn-light text-secondary" onClick={e => this.chooseOption(option)} key={option}>{option}</button>
             )}
-            <div>{_.range(0, kanaIndex).map(i => 
+            <div>&nbsp;{_.range(0, kanaIndex).map(i => 
                 <span>{kana[i]} {wanakana.toRomaji(kana[i])}</span>
             )}</div>
         </div>
