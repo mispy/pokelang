b7f3df64084bccdbd4516b77b98291bf87801e38
Jaiden Mispy <jaiden@mispy.me>
2018-07-08 17:37:18 +0800

    Add sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -22,12 +22,14 @@ class Pokemon extends React.Component<{ number: number, out?: boolean, hidden?: 
     }
 }
 
+
+
 // TODO handle context dependent modifiers like "ー"
 function splitKana(jp: string): string[] {
     const kana = []
 
     const beforeModifiers = ["ッ"]
-    const afterModifiers = ["ー",  "ャ", "ョ", "ュ", "ェ"]
+    const afterModifiers = ["ー", "ャ", "ョ", "ュ", "ェ"]
     
     for (let i = 0; i < jp.length; i++) {
         let j = i
@@ -49,6 +51,10 @@ function splitKana(jp: string): string[] {
     return kana
 }
 
+function InlineButton(props: { active: boolean, children: any, onClick: any }) {
+    return <button className={`inlineToggle${props.active ? " active" : ""}`} onClick={props.onClick}>{props.children}</button>
+}
+
 @observer
 class Main extends React.Component {
     @observable prevPoke?: number
@@ -57,9 +63,14 @@ class Main extends React.Component {
     @observable nextPoke: number = _.random(1, MAX_POKEMON)
     @observable hintCount: number = 0
     @observable kanaIndex: number = 0
+    @observable kanaMode: 'katakana'|'hiragana' = 'katakana'
 
     @computed get japanese(): string {
-        return pokenames.ja[this.poke-1]
+        const katakana = pokenames.ja[this.poke-1]
+        if (this.kanaMode === 'hiragana')
+            return wanakana.toHiragana(katakana)
+        else
+            return katakana
     }
 
     @computed get kana(): string[] {
@@ -102,7 +113,7 @@ class Main extends React.Component {
     }
 
     render() {
-        const {poke, prevPoke, nextPoke, japanese, kana, options, currentKana, kanaIndex} = this
+        const {poke, prevPoke, nextPoke, japanese, kana, options, currentKana, kanaIndex, kanaMode} = this
         /*for (const name of pokenames.ja) {
             const trueRomaji = wanakana.toRomaji(name)
             const splitRomaji = splitKana(name).map(kana => wanakana.toRomaji(kana)).join("")
@@ -110,19 +121,30 @@ class Main extends React.Component {
                 console.log(name, trueRomaji, splitRomaji)
         }*/
 
-        return <div className="container text-center">
-            <div className="runway">
-                <Pokemon number={poke} key={this.question}/>
-                {prevPoke && <Pokemon number={prevPoke} out={true} key={`prev-${this.question}`}/>}
-                {nextPoke && <Pokemon number={nextPoke} key={`next-${this.question}`} hidden={true}/>}
+        return <div className="app">
+            <div className="sidebar">
+                <h1>Jolteon's Adventures</h1>
+                <p>Jolteon and his Pokémon friends are traveling the world to learn new languages. Can you help them to read their Japanese names in <InlineButton active={this.kanaMode === 'katakana'} onClick={action(e => this.kanaMode = 'katakana')}>katakana</InlineButton> and <InlineButton active={this.kanaMode ==='hiragana'} onClick={action(e => this.kanaMode = 'hiragana')}>hiragana</InlineButton>?</p>
+
+                <hr/>
+                <small>Created by <a href="https://mispy.me">Jaiden Mispy</a>. Dedicated to my trusty travel companion, <a href="/fizz.jpg">Fission the Jolteon</a>.</small><br/>
+                <hr/>
+                <small><a href="https://github.com/mispy/kanajolt">https://github.com/mispy/kanajolt</a></small>
             </div>
-            <div>{currentKana}</div>
-            {options.map(option => 
-                <button className="btn btn-light text-secondary" onClick={e => this.chooseOption(option)} key={option}>{option}</button>
-            )}
-            <div>&nbsp;{_.range(0, kanaIndex).map(i => 
-                <span>{kana[i]} {wanakana.toRomaji(kana[i])}</span>
-            )}</div>
+            <main className="container text-center">
+                <div className="runway">
+                    <Pokemon number={poke} key={this.question}/>
+                    {prevPoke && <Pokemon number={prevPoke} out={true} key={`prev-${this.question}`}/>}
+                    {nextPoke && <Pokemon number={nextPoke} key={`next-${this.question}`} hidden={true}/>}
+                </div>
+                <div>{currentKana}</div>
+                {options.map(option => 
+                    <button className="btn btn-light text-secondary romaji" onClick={e => this.chooseOption(option)} key={option}>{option}</button>
+                )}
+                <div>&nbsp;{_.range(0, kanaIndex).map(i => 
+                    <span>{kana[i]} {wanakana.toRomaji(kana[i])}</span>
+                )}</div>
+            </main>
         </div>
     }
 }
