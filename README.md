e87881cc2dfdf3f2907ad3f503b195948d87743b
Jaiden Mispy <jaiden@mispy.me>
2018-07-19 01:22:12 +0800

    Working on ending

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A little game I made to help learn the Japanese kana: https://kanajolt.mispy.me/
 
 ### Design notes
 
-I started by considering whether I could write a very simple program that would show me a character and tell me if I identified it correctly, for my own practice. Initially I thought about just making an interactive CLI thing, before remembering that static web apps are about as easy as that, and then I could share it as well.
+I started thinking about whether I could write a simple program that would show me a character and tell me if I identified it correctly, for my own practice. Initially I thought about just making an interactive CLI thing, before remembering that static web apps are about as easy as that, and then I could share it as well.
 
-The Pokémon theme came from considering whether I could give it a cute context to aid memorization. Making something a Pokémon fan project gives you access to both a shared cultural space and a nice dataset of names, sprites, statistics and ideas to play with, so it's often been my default for prototyping.
+The Pokémon theme came from wondering whether I could give it a cute context to aid memorization. Making something a Pokémon fan project gives you access to both a shared cultural space and a nice dataset of names, sprites, statistics and ideas to play with, so it's often been my default for prototyping.
 
-The initial prototype simply asked you to type the romaji transliteration of each name. This was useful for practicing speed, but not learning!
+The initial prototype simply asked you to type the romaji transliteration of each name. This was useful for practicing speed, but not learning, and didn't work well on mobile! So after testing with a friend I switched to a multiple choice input inspired by Duolingo.
--- src/index.tsx
+++ src/index.tsx
@@ -35,9 +35,6 @@ class Pokemon extends React.Component<{ number: number, out?: boolean, hidden?: 
     }
 }
 
-
-
-// TODO handle context dependent modifiers like "ー"
 function splitKana(jp: string): string[] {
     const kana = []
 
@@ -114,6 +111,72 @@ class GameState {
         const newGame = new GameState()
         newGame.save()
         this.load()
+    }
+}
+
+function getFauxBBox(path: SVGPathElement) {
+  const pathlen = path.getTotalLength()
+  const step = pathlen/100
+  
+  let left = 0
+  let top = 0
+  let right = 0
+  let bottom = 0
+
+  for (let i = 0; i < 100; i++) {
+    const point = path.getPointAtLength(step*i)
+    if (point.x < left) left = point.x
+    if (point.x > right) right = point.x
+    if (point.y < top) top = point.y
+    if (point.y > bottom) bottom = point.y
+  }
+
+  return { left: left, top: top, right: right, bottom: bottom, width: right-left, height: bottom-top }
+}
+
+@observer
+class Ending extends React.Component {
+    base: HTMLDivElement
+    componentDidMount() {
+        const pathSpec = "M 297.29747,550.86823 C 283.52243,535.43191 249.1268,505.33855 220.86277,483.99412 C 137.11867,420.75228 125.72108,411.5999 91.719238,380.29088 C 29.03471,322.57071 2.413622,264.58086 2.5048478,185.95124 C 2.5493594,147.56739 5.1656152,132.77929 15.914734,110.15398 C 34.151433,71.768267 61.014996,43.244667 95.360052,25.799457 C 119.68545,13.443675 131.6827,7.9542046 172.30448,7.7296236 C 214.79777,7.4947896 223.74311,12.449347 248.73919,26.181459 C 279.1637,42.895777 310.47909,78.617167 316.95242,103.99205 L 320.95052,119.66445 L 330.81015,98.079942 C 386.52632,-23.892986 564.40851,-22.06811 626.31244,101.11153 C 645.95011,140.18758 648.10608,223.6247 630.69256,270.6244 C 607.97729,331.93377 565.31255,378.67493 466.68622,450.30098 C 402.0054,497.27462 328.80148,568.34684 323.70555,578.32901 C 317.79007,589.91654 323.42339,580.14491 297.29747,550.86823 z"
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+        path.setAttribute('d', pathSpec)
+        path.style['strokeWidth'] = '2'
+        path.style['stroke'] = 'none'
+        path.style['fill'] = 'none'
+    
+        const length = path.getTotalLength()
+        const bbox = getFauxBBox(path)
+
+        const bounds = (this.base.parentNode as any).getBoundingClientRect()
+        const scale = Math.min(bounds.width/bbox.width, bounds.height/bbox.height)
+
+        this.base.style['width'] = `${bbox.width*scale}px`
+        this.base.style['height'] = `${bbox.height*scale}px`
+
+        const imgs = this.base.querySelectorAll("img")
+        const interdist = length / imgs.length
+        for (let i = 0; i < imgs.length; i++) {
+            const img = imgs[i]
+            const dest = path.getPointAtLength(i*interdist)
+
+            const tx = scale * dest.x - 16
+            const ty = scale * dest.y - 16
+            img.style.left = `${tx}px`
+            img.style.top = `${ty}px`
+        }
+    }
+
+    render() {
+
+
+        return <div className="ending" ref={e => this.base = (e as any)}>
+            {_.range(0, 50).map(i => {
+                return <img style={{ position: 'absolute', left: 0, top: 0}} src="../overworld/right/135.png"/>
+            })}
+            <div>Fin</div>
+        </div>
     }
 }
 
@@ -241,6 +304,7 @@ class Main extends React.Component {
     }
     
     renderMain() {
+        //return <main><Ending/></main>
         const {poke, prevPoke, nextPoke, kana, options, currentKana, wrongChoices, numNamed, hintText} = this
         const {game} = this
         const {kanaIndex, streakCounter} = this.game
